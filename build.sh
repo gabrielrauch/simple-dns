7f0df3abdc991c3efbb1632acebb2ea5a2e9948e
gabrielrauch <gabrielrauch@protonmail.com>
2023-11-02 17:30:07 -0300

    Build.sh correto

--- build.sh
+++ build.sh
@@ -1,4 +1,13 @@
-mkdir build  
-cd build     
-cmake ..     
-make        
+#!/bin/bash
+
+mkdir -p build
+cd build
+cmake ..
+make
+
+if [ $? -eq 0 ]; then
+    echo "Compilação bem-sucedida. Para executar o servidor DNS, use o comando:"
+    echo "./dns_server"
+else
+    echo "Erro durante a compilação. Certifique-se de que está em um abiente UNIX."
+fi
