3e3f621ad194ac38a28e67584c48b1d976d7b09f
gabrielrauch <gabrielrauch@protonmail.com>
2023-11-02 19:45:13 -0300

    Lógicas faltantes implementadas

--- src/dns_server.cpp
+++ src/dns_server.cpp
@@ -4,11 +4,16 @@
 #include <unistd.h>
 #include <cstring>
 #include <arpa/inet.h>
+#include <netdb.h>
+#include <mutex>
 #include <netinet/in.h>
+#include <condition_variable>
+#include <regex>
 
 DNS_Server::DNS_Server()
 {
     socket_fd = -1;
+    validDomainPattern = std::regex("^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9]\\.[a-zA-Z]{2,}$");
 }
 
 DNS_Server::~DNS_Server()
@@ -53,10 +58,91 @@ bool DNS_Server::LoadDNSRecords(const std::string &configFileName)
     std::string line;
     while (std::getline(configFile, line))
     {
+        std::string domain;
+        std::string ip;
+
+        std::istringstream lineStream(line);
+        lineStream >> domain >> ip;
+
+        if (!domain.empty() && !ip.empty())
+        {
+            AddToCache(domain, ip);
+        }
     }
 
     configFile.close();
     return true;
+}
+
+bool DNS_Server::IsValidDomain(const std::string &domain)
+{
+    return std::regex_match(domain, validDomainPattern);
+}
+
+bool DNS_Server::IsQuerySizeValid(const ssize_t querySize)
+{
+    return querySize < 1024;
+}
+
+std::string DNS_Server::LookupInCache(const std::string &domain)
+{
+    std::lock_guard<std::mutex> lock(cacheMutex);
+    auto it = dnsCache.find(domain);
+    if (it != dnsCache.end())
+    {
+        return it->second;
+    }
+    return "";
+}
+
+void DNS_Server::AddToCache(const std::string &domain, const std::string &ip)
+{
+    std::lock_guard<std::mutex> lock(cacheMutex);
+    dnsCache[domain] = ip;
+}
+
+std::string DNS_Server::ProcessDNSQuery(const std::string &query)
+{
+    std::string domain;
+    std::string recordType;
+
+    size_t spacePos = query.find(" ");
+    if (spacePos != std::string::npos)
+    {
+        domain = query.substr(0, spacePos);
+        recordType = query.substr(spacePos + 1);
+    }
+    else
+    {
+        return "";
+    }
+
+    if (recordType == "A")
+    {
+        struct addrinfo hints;
+        struct addrinfo *result, *rp;
+        int ret;
+
+        memset(&hints, 0, sizeof(struct addrinfo));
+        hints.ai_family = AF_INET; // IPv4
+        hints.ai_socktype = SOCK_STREAM;
+
+        ret = getaddrinfo(domain.c_str(), NULL, &hints, &result);
+        if (ret == 0)
+        {
+            for (rp = result; rp != NULL; rp = rp->ai_next)
+            {
+                struct sockaddr_in *addr = (struct sockaddr_in *)rp->ai_addr;
+                std::string ip = inet_ntoa(addr->sin_addr);
+
+                freeaddrinfo(result);
+
+                return ip;
+            }
+        }
+    }
+
+    return "";
 }
 
 bool DNS_Server::Start()
@@ -74,6 +160,40 @@ bool DNS_Server::Start()
         char buffer[1024];
 
         ssize_t bytes_received = recvfrom(socket_fd, buffer, sizeof(buffer), 0, (struct sockaddr *)&client_address, &client_address_len);
+
+        if (!IsQuerySizeValid(bytes_received))
+        {
+            std::cerr << "Tamanho da query excedido, ignorando..." << std::endl;
+            continue;
+        }
+
+        std::string query = std::string(buffer, bytes_received);
+        if (!IsValidDomain(query))
+        {
+            std::cerr << "Query inválida, ignorando..." << std::endl;
+        }
+
+        std::string cachedResult = LookupInCache(query);
+
+        if (!cachedResult.empty())
+        {
+            sendto(socket_fd, cachedResult.c_str(), cachedResult.size(), 0, (struct sockaddr *)&client_address, client_address_len);
+        }
+        else
+        {
+            std::string queryResult = ProcessDNSQuery(query);
+
+            if (!queryResult.empty())
+            {
+                AddToCache(query, queryResult);
+
+                sendto(socket_fd, queryResult.c_str(), queryResult.size(), 0, (struct sockaddr *)&client_address, client_address_len);
+            }
+            else
+            {
+                std::cerr << "Consulta DNS não encontrada ou erro na pesquisa DNS." << std::endl;
+            }
+        }
     }
 
     return true;
@@ -81,4 +201,11 @@ bool DNS_Server::Start()
 
 void DNS_Server::WaitForShutdown()
 {
+    std::mutex mutex;
+    std::unique_lock<std::mutex> lock(mutex);
+    std::condition_variable cv;
+
+    cv.wait_for(lock, std::chrono::seconds(30));
+
+    std::cout << "Encerrando o servidor..." << std::endl;
 }
--- src/dns_server.h
+++ src/dns_server.h
@@ -5,27 +5,43 @@
 #include <vector>
 #include <map>
 #include <netinet/in.h>
+#include <unordered_map>
+#include <mutex>
+#include <regex>
 
 class DNS_Server
 {
+
+private:
+    int socket_fd;
+    struct sockaddr_in server_address;
+    std::map<std::string, std::string> dns_records;
+    std::unordered_map<std::string, std::string> dnsCache;
+    std::mutex cacheMutex;
+    std::regex validDomainPattern;
+
 public:
     DNS_Server();
+
     ~DNS_Server();
 
     bool Initialize(const std::string &ip, int port);
 
     bool LoadDNSRecords(const std::string &configFileName);
 
+    bool IsValidDomain(const std::string &domain);
+
+    bool IsQuerySizeValid(const ssize_t querySize);
+
     bool Start();
 
     void WaitForShutdown();
 
-private:
-    int socket_fd;
-    struct sockaddr_in server_address;
-    std::map<std::string, std::string> dns_records;
+    std::string LookupInCache(const std::string &domain);
 
-    void HandleDNSRequest(int client_socket);
+    void AddToCache(const std::string &domain, const std::string &ip);
+
+    std::string ProcessDNSQuery(const std::string &query);
 };
 
 #endif
