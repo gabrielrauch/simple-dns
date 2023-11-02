55e96a93bea674ab66c314cdb4a0fabfc2450354
gabrielrauch <gabrielrauch@protonmail.com>
2023-11-02 20:47:41 -0300

    Build consertado

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ make
 
 if [ $? -eq 0 ]; then
     echo "Compilação bem-sucedida. Para executar o servidor DNS, use o comando:"
-    echo "./dns_server"
+    echo "./simple_dns"
 else
     echo "Erro durante a compilação. Certifique-se de que está em um abiente UNIX."
 fi
